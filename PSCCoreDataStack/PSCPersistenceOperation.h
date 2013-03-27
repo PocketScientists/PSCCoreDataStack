c08e5454ad341232ec49bed3aec234a5493bf6c4
Matthias Tretter <matthias.tretter@gmail.com>
2013-03-27 10:39:15 +0100

    NSOperation/GCD for simple persistence actions

---
+++ PSCCoreDataStack/PSCPersistenceOperation.h
@@ -0,0 +1,38 @@
+//
+//  PSCPersistenceOperation.h
+//  PSCCoreDataStack
+//
+//  Created by Matthias Tretter on 26.03.13.
+//  Copyright (c) 2013 PocketScience. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+typedef void(^psc_persistence_block)(NSManagedObjectContext *localContext);
+
+// the queue used in persistDataInBackgroundWithParentContext:block:completion:
+dispatch_queue_t psc_persistence_queue(void);
+
+
+@interface PSCPersistenceOperation : NSOperation
+
+/**
+ Uses GCD to perform a save operation on a background queue and not NSOperation.
+ The completion block is called on the main queue.
+ */
++ (void)persistDataInBackgroundWithParentContext:(NSManagedObjectContext *)parentContext
+                                           block:(psc_persistence_block)block
+                                      completion:(dispatch_block_t)completion;
+
+/**
+ Creates an NSOperation subclass that can be used to persist data to a local context
+ */
++ (instancetype)operationWithParentContext:(NSManagedObjectContext *)parentContext
+                                     block:(psc_persistence_block)block
+                                completion:(dispatch_block_t)completion;
+
+/** Subclasses can override to perform a persistence action */
+- (void)persistWithContext:(NSManagedObjectContext *)localContext;
+
+@end
---
+++ PSCCoreDataStack/PSCPersistenceOperation.m
@@ -0,0 +1,104 @@
+#import "PSCPersistenceOperation.h"
+#import "PSCCoreDataStack.h"
+#import "NSManagedObjectContext+PSCCoreDataHelper.h"
+
+
+static dispatch_queue_t _psc_persistence_queue = NULL;
+
+
+@interface PSCPersistenceOperation ()
+
+@property (nonatomic, strong) NSManagedObjectContext *parentContext;
+@property (nonatomic, copy) psc_persistence_block persistenceBlock;
+
+@end
+
+
+@implementation PSCPersistenceOperation
+
+////////////////////////////////////////////////////////////////////////
+#pragma mark - Lifecycle
+////////////////////////////////////////////////////////////////////////
+
++ (instancetype)operationWithParentContext:(NSManagedObjectContext *)parentContext
+                                     block:(psc_persistence_block)block
+                                completion:(dispatch_block_t)completion {
+    NSParameterAssert(parentContext != nil);
+    NSParameterAssert(block != nil);
+
+    PSCPersistenceOperation *operation = [[self alloc] init];
+
+    operation.parentContext = parentContext;
+    operation.persistenceBlock = block;
+    operation.completionBlock = completion;
+
+    return operation;
+}
+
++ (void)persistDataInBackgroundWithParentContext:(NSManagedObjectContext *)parentContext
+                                           block:(psc_persistence_block)block
+                                      completion:(dispatch_block_t)completion {
+    NSParameterAssert(parentContext != nil);
+    NSParameterAssert(block != nil);
+
+    dispatch_async(psc_persistence_queue(), ^{
+        NSManagedObjectContext *localContext = [parentContext newChildContextWithConcurrencyType:NSConfinementConcurrencyType];
+        NSError *error = nil;
+
+        if (block != nil) {
+            block(localContext);
+        }
+
+        if (![localContext save:&error]) {
+            NSLog(@"Error persisting local context in PSCPersistenceAction: %@", error);
+        }
+
+        if (completion != nil) {
+            dispatch_async(dispatch_get_main_queue(), completion);
+        }
+    });
+}
+
+////////////////////////////////////////////////////////////////////////
+#pragma mark - PSCPersisteceOperation
+////////////////////////////////////////////////////////////////////////
+
+- (void)persistWithContext:(NSManagedObjectContext *)localContext {
+    // do nothing, subclasses can override
+}
+
+////////////////////////////////////////////////////////////////////////
+#pragma mark - NSOperation
+////////////////////////////////////////////////////////////////////////
+
+- (void)main {
+    NSManagedObjectContext *localContext = [self.parentContext newChildContextWithConcurrencyType:NSConfinementConcurrencyType];
+    NSError *error = nil;
+
+    // either persist via block (if set), or call method in subclass
+    if (self.persistenceBlock != nil) {
+        self.persistenceBlock(localContext);
+    } else {
+        [self persistWithContext:localContext];
+    }
+
+    if (![localContext save:&error]) {
+        NSLog(@"Error persisting local context in PSCPersistenceAction: %@", error);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////
+#pragma mark - Functions
+////////////////////////////////////////////////////////////////////////
+
+dispatch_queue_t psc_persistence_queue(void) {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _psc_persistence_queue = dispatch_queue_create("com.pocketscience.persistence-queue", 0);
+    });
+
+    return _psc_persistence_queue;
+}
+
+
+@end
